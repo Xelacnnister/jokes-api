2b071666b290d203a5740cade53b25f3eede3a18
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-05-19 19:22:12 +0200

    added logic of the weather message

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const apiUrl = "https://icanhazdadjoke.com/";
+const jokeApiUrl = "https://icanhazdadjoke.com/";
+const weatherApiUrl = "https://www.el-tiempo.net/api/json/v2/provincias/08"
 
+ 
 
 document.getElementById("jokeButton").addEventListener("click", getJoke);
 
 async function getJoke(){
-    await fetch(apiUrl, { 
+    await fetch(jokeApiUrl, { 
         method: "GET",
         headers: {'Accept': 'application/json'} })
     .then((resp) => resp.json())
-    .then(data => console.log(data.joke))
+    .then(data => document.getElementById("output").innerHTML = data.joke);  
 }
+
+async function weather(){
+    await fetch(weatherApiUrl, {
+        method: "GET"
+    })
+    .then((resp) => resp.json())
+    // .then(data => console.log(data.today));
+    .then(data => document.getElementById("weather").innerHTML = data.today.p)  
+    // .then(data => document.getElementById("weather").innerHTML = data.today.p);  
+}
+
+weather();
+
+//.provincias[7]
