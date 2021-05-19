cc107bb06c9880e4ca87e60624dc07401b8e9a35
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-05-19 16:00:54 +0200

    addEventListener get joke when button is pressed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const apiUrl = "https://icanhazdadjoke.com/";
 
+
+document.getElementById("jokeButton").addEventListener("click", getJoke);
+
 async function getJoke(){
     await fetch(apiUrl, { 
         method: "GET",
         headers: {'Accept': 'application/json'} })
     .then((resp) => resp.json())
     .then(data => console.log(data.joke))
-
-
 }
-
-getJoke();
